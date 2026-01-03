c3cf61ca20aa816bba21e714b04cca1531af4ed0
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-01-03 11:07:29 -0300

    feat: constantes para o projeto

---
+++ backend/constants/banco_dados.py
@@ -0,0 +1,11 @@
+BANCO_DADOS_USUARIOS = "database/usuarios.db"
+TABELA_USUARIOS = "usuarios"
+
+BANCO_DADOS_PRODUTOS = "database/produtos.db"
+TABELA_PRODUTOS = "produtos"
+
+BANCO_DADOS_CONTAGENS = "database/contagens.db"
+TABELA_CONTAGENS = "contagens"
+
+BANCO_DADOS_CONTAGENS_TEMPORARIAS = "database/contagens_temporarias.db"
+TABELA_CONTAGENS_TEMPORARIAS = "contagens_temporarias"

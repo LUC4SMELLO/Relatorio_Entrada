3285295aeeb9c6ffbe508254c7a1d78081ed3f0c
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-01-03 11:09:43 -0300

    feat: banco dados usuários

---
+++ database/banco_dados_usuarios.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+from backend.constants.banco_dados import BANCO_DADOS_USUARIOS, TABELA_USUARIOS
+
+def conectar_banco_dados_usuarios():
+
+    return sqlite3.connect(BANCO_DADOS_USUARIOS)
+
+def criar_banco_dados_usuarios():
+
+    conexao = conectar_banco_dados_usuarios()
+    cursor = conexao.cursor()
+
+    cursor.execute(
+    f"""
+    CREATE TABLE IF NOT EXISTS {TABELA_USUARIOS} (
+    usuario_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome_completo VARCHAR(50),
+    senha VARCHAR(50)
+    )
+    """
+    )
+
+    conexao.commit()
+    conexao.close()

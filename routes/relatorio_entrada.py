707849e9a64f1c5fadbd27b76ed36c9ca99808a4
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-01-04 13:50:45 -0300

    feat: rota para salvar os relatórios

--- routes/relatorio_entrada.py
+++ routes/relatorio_entrada.py
@@ -12,7 +12,7 @@ relatorio_entrada_bp = Blueprint("relatorio_entrada", __name__)
 @relatorio_entrada_bp.route("/relatorio_entrada", methods=["GET"])
 def relatorio_entrada():
 
-    return render_template("relatorio_entrada.html")
+    return render_template("relatorio_entrada_2.html")
 
 
 
@@ -33,3 +33,45 @@ def carga():
         return jsonify({"produtos": []})
 
     return jsonify(carga)
+
+
+@relatorio_entrada_bp.route("/relatorio_entrada/salvar", methods=["POST"])
+def salvar_relatorio():
+    dados = request.json
+
+    print("DADOS RECEBIDOS:")
+    print(dados)
+
+    transporte = dados["transporte"]
+
+    for produto in dados["produtos"]:
+        codigo = produto["codigo"]
+        descricao = produto["descricao"]
+
+        for lote in produto["lotes"]:
+            quantidade = int(lote["quantidade"])
+            estoque = lote["estoque"]
+            fabricacao = lote["fabricacao"]
+            vencimento = lote["vencimento"]
+
+            alterar_fefo = lote["alterar_fefo"]
+            pallet_danificado = lote["pallet_danificado"]
+            vazamento = lote["vazamento"]
+            observacao = lote["observacao"]
+
+        print(transporte)
+        print(codigo)
+        print(descricao)
+        print(quantidade)
+        print(estoque)
+        print(fabricacao)
+        print(vencimento)
+        print(alterar_fefo)
+        print(pallet_danificado)
+        print(vazamento)
+        print(observacao)
+    
+
+    # SALVAR NO BANCO DE DADOS
+
+    return jsonify({"status": "ok"})

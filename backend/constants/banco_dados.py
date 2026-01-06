8824e0388861daf87e335446fa5d6a9052f17c63
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-01-05 21:14:18 -0300

    feat: nova constante banco dados

--- backend/constants/banco_dados.py
+++ backend/constants/banco_dados.py
@@ -3,3 +3,6 @@ TABELA_USUARIOS = "usuarios"
 
 BANCO_DADOS_RELATORIOS = "database/relatorios.db"
 TABELA_RELATORIOS = "relatorios"
+
+BANCO_DADOS_RELATORIOS_TEMPORARIOS = "database/relatorios_temporarios.db"
+TABELA_RELATORIOS_TEMPORARIOS = "relatorios_temporarios"

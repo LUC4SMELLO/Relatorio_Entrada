a956ce1884d0c3c2bd5032a68a3fc827a14ddc94
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-01-06 16:21:46 -0300

    feat: adicionando 3 novas colunas banco dados relatórios

--- database/banco_dados_relatorios.py
+++ database/banco_dados_relatorios.py
@@ -15,21 +15,48 @@ def criar_tabela_relatorios():
     cursor.execute(
     f"""
     CREATE TABLE IF NOT EXISTS {TABELA_RELATORIOS} (
-    transporte INTERGER,
-    origem VARCHAR(50),
-    usuario_id VARCHAR(10),
-    nome_usuario VARCHAR(250),
-    data_relatorio VARCHAR(10),
-    data_carga VARCHAR(10),
-    codigo_produto VARCHAR(10),
-    quantidade INTERGER,
-    data_estoque VARCHAR(10),
-    data_fabricacao VARCHAR(10),
-    data_vencimento VARCHAR(10),
-    alterar_fefo VARCHAR(5),
-    pallet_danificado VARCHAR(5),
-    vazamento VARCHAR(5),
-    observacao VARCHAR(250)
+        transporte INTEGER,
+        origem VARCHAR(50),
+        usuario_id VARCHAR(10),
+        nome_usuario VARCHAR(250),
+        data_relatorio VARCHAR(10),
+        data_carga VARCHAR(10),
+        codigo_produto VARCHAR(10),
+        quantidade INTEGER,
+        data_estoque VARCHAR(10),
+        data_fabricacao VARCHAR(10),
+        data_vencimento VARCHAR(10),
+        alterar_fefo VARCHAR(5),
+        pallet_danificado VARCHAR(5),
+        vazamento VARCHAR(5),
+        observacao VARCHAR(250),
+        
+
+        -- 1. SHELF LIFE (Vencimento - Fabricação)
+        shelf_life INTEGER GENERATED ALWAYS AS (
+            julianday(substr(data_vencimento, 7, 4) || '-' || substr(data_vencimento, 4, 2) || '-' || substr(data_vencimento, 1, 2)) -
+            julianday(substr(data_fabricacao, 7, 4) || '-' || substr(data_fabricacao, 4, 2) || '-' || substr(data_fabricacao, 1, 2))
+        ) VIRTUAL,
+
+        -- 2. DIAS PARA VENCIMENTO (Vencimento - Chegada/Estoque)
+        dias_para_vencimento INTEGER GENERATED ALWAYS AS (
+            julianday(substr(data_vencimento, 7, 4) || '-' || substr(data_vencimento, 4, 2) || '-' || substr(data_vencimento, 1, 2)) -
+            julianday(substr(data_relatorio, 7, 4) || '-' || substr(data_relatorio, 4, 2) || '-' || substr(data_relatorio, 1, 2))
+        ) VIRTUAL,
+
+        -- 3. % DIAS VENCIMENTO (Dias p/ Vencer - Shelf Life * 100)
+        percentual_dias_vencimento REAL GENERATED ALWAYS AS (
+        ROUND(
+            ABS(
+                ((julianday(substr(data_vencimento, 7, 4) || '-' || substr(data_vencimento, 4, 2) || '-' || substr(data_vencimento, 1, 2)) -
+                  julianday(substr(data_relatorio, 7, 4) || '-' || substr(data_relatorio, 4, 2) || '-' || substr(data_relatorio, 1, 2))) * 100.0) /
+                NULLIF(
+                    julianday(substr(data_vencimento, 7, 4) || '-' || substr(data_vencimento, 4, 2) || '-' || substr(data_vencimento, 1, 2)) -
+                    julianday(substr(data_fabricacao, 7, 4) || '-' || substr(data_fabricacao, 4, 2) || '-' || substr(data_fabricacao, 1, 2)), 
+                0)
+            ), 
+        2) -- O número 2 define as casas decimais
+    ) VIRTUAL
     )
     """
     )

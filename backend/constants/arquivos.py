86d74655fd7d717500c834271ee148734c693cd0
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-01-06 11:06:05 -0300

    feat: dicionário para fábricas

--- backend/constants/arquivos.py
+++ backend/constants/arquivos.py
@@ -1,1 +1,15 @@
 CAMINHO_PEDIDOS_EXCEL = "arquivos/PEDIDOS CTA DEZ-25.xlsm"
+
+DICIONARIO_FABRICAS = {
+    "1": "Itabirito",
+    "2": "Três Corações",
+    "3": "Mogi das Cruzes",
+    "4": "Betim",
+    "5": "Betim",
+    "6": "Três Corações",
+    "7": "Aparecida",
+    "8": "OPL Smart",
+    "9": "Itabirito",
+    "10": "OPL Smart",
+    "11": "Fountain Moji"
+}
